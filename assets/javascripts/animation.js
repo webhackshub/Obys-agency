8c636238abe173ad7570b2513c12e99e0d15aed4
Suraj Yaduvanshi <pvt.suraj37@gmail.com>
2024-09-19 05:53:14 +0530

    Homepage completed

--- assets/javascripts/animation.js
+++ assets/javascripts/animation.js
@@ -1,17 +1,43 @@
-gsap.from(".text h1",{
-  y: 150,
-  stagger: .25
-})
+const loaderAnimation = () => {
+  const tl = gsap.timeline();
 
-gsap.to(".loader",{
-  delay: 4,
-  y: -1500,
-  opacity: 0,
-  duration: 1
-})
+  gsap.from(".text h1", {
+    y: 150,
+    stagger: 0.25,
+  });
 
-gsap.to(".text",{
-  delay: 3,
-  opacity: 0,
-  stagger: .25
-})
+  gsap.to(".loader", {
+    delay: 4,
+    y: -1500,
+    opacity: 0,
+    duration: 1.5,
+  });
+
+  tl.to(".text", {
+    delay: 3,
+    opacity: 0,
+    stagger: 0.25,
+  });
+
+  tl.from(".home-text h1", {
+    y: 120,
+    stagger: 0.15,
+  });
+};
+// loaderAnimation();
+
+const mouseCursor = () => {
+  const website = document.querySelector("body");
+  website.addEventListener("mousemove", function (dets) {
+    gsap.to(".cursor", {
+      top: dets.y,
+      left: dets.x,
+    });
+  });
+};
+mouseCursor();
+
+const makeMagnet = () => {
+  Shery.makeMagnet(".nav-magnet");
+};
+makeMagnet();
--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,10 +1,26 @@
-let count = 0;
-let counter = document.querySelector(".counter");
-let countInterval = setInterval(() => {
-  if (count < 100) {
-    counter.textContent = count++;
+const loader = () => {
+  let count = 0;
+  let counter = document.querySelector(".counter");
+  let countInterval = setInterval(() => {
+    if (count < 100) {
+      counter.textContent = count++;
+    } else {
+      counter.textContent = count;
+      clearInterval(countInterval);
+    }
+  }, 25);
+};
+// loader();
+
+const reelController = () => {
+  const controllerCount = 0;
+  const controllerPlay = document.querySelector(".play");
+  const controllerPause = document.querySelector(".pause");
+
+  if (controllerCount === 0) {
+    controllerPause.style.display = "none";
   } else {
-    counter.textContent = count;
-    clearInterval(countInterval);
+    controllerPlay.style.display = "none";
   }
-}, 25);
+};
+reelController();
